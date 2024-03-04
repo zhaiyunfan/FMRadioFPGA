70996823097274b88408ef770b3e254eb9e7fc71
zhaiyunfan <zhaiyunfan20000324@qq.com>
2024-03-04 01:26:30 -0600

    clean the project

--- sv/mul.sv
+++ sv/mul.sv
@@ -1,18 +1,3 @@
-function logic[31:0] QUANTIZE_I; 
-input logic[31:0] i;
-    begin
-        return $signed(i) <<< 10;
-    end
-endfunction
-
-function logic[31:0] DEQUANTIZE; 
-input logic[31:0] i;
-    begin
-        return $signed(i) >>> 10;
-    end
-endfunction
-
-
 function logic[31:0] mul;
 input  logic [31:0] x_in;
 input  logic [31:0] y_in;
